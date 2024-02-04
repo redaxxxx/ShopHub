03ad9750a24b10cb106690310bae7bfc7cea9915
Reda Nabil <redanabil518@gmail.com>
2024-02-04 21:28:09 +0200

    setup bottom_nav and viewPager

--- app/src/main/java/com/android/developer/prof/reda/shophub/activities/ShoppingActivity.kt
+++ app/src/main/java/com/android/developer/prof/reda/shophub/activities/ShoppingActivity.kt
@@ -2,11 +2,22 @@ package com.android.developer.prof.reda.shophub.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.android.developer.prof.reda.shophub.R
+import com.android.developer.prof.reda.shophub.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        val binding = DataBindingUtil.setContentView<ActivityShoppingBinding>(this, R.layout.activity_shopping)
+
+        val navController by lazy {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+            navHostFragment.navController
+        }
+
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
